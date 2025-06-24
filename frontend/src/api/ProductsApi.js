4be503f9484b98ef0ccd1bce58921039b574d1ae
gmirtskhulava <mgiorgi685@gmail.com>
2025-06-25 00:44:40 +0400

    Deploy test [!]

--- frontend/src/api/ProductsApi.js
+++ frontend/src/api/ProductsApi.js
@@ -3,19 +3,19 @@ import axios from 'axios';
 const base_url = `https://pronia-mziuri.onrender.com`
 
 export const getProducts = async () => {
-    const response = await axios.get(`$${base_url}/api/products/get-products`, {
+    const response = await axios.get(`${base_url}/api/products/get-products`, {
         withCredentials: true,
     });
     return response;
 };
 export const getProductById = async (productId) => {
-    const response = await axios.get(`$${base_url}/api/products/get-product-id`, {
+    const response = await axios.get(`${base_url}/api/products/get-product-id`, {
         headers: { Authorization: productId },
     });
     return response;
 };
 export const getProductByName = async (productName) => {
-    const response = await axios.get(`$${base_url}/api/products/get-product-name`, {
+    const response = await axios.get(`${base_url}/api/products/get-product-name`, {
         params: { name: productName },
     });
     return response;
